50eff3e163301ba3a012cb0714eea1a27392ac55
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-02-02 00:15:35 -0600

    mang test

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,62 +1,14 @@
-const Manager = require("./lib/Manager.js.js");
+const Manager = require('../lib/Manager');
 const Employee = require('../lib/Employee');
 
+test("creates an Manager office number", () => {
+  const mang = new Manager("Cam", 6, "fredready@fakeemail.com", "Manager", 23);
 
-
-jest.mock('../lib/Potion.js');
-
-test('creates an enemy object', () => {
-  const enemy = new Enemy('goblin', 'sword');
-
-  expect(enemy.name).toBe('goblin');
-  expect(enemy.weapon).toBe('sword');
-  expect(enemy.health).toEqual(expect.any(Number));
-  expect(enemy.strength).toEqual(expect.any(Number));
-  expect(enemy.agility).toEqual(expect.any(Number));
-  expect(enemy.potion).toEqual(expect.any(Object));
+  expect(mang.officeNumber).toBe(100);
 });
 
-test("gets enemy's health value", () => {
-  const enemy = new Enemy('goblin', 'sword');
+test("get Role()", () => {
+  const mang = new Manager("Cam", 6, "fredready@fakeemail.com", "Manager", 23);
 
-  expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+  expect(mang.getRole()).toBe("Manager");
 });
-
-test('gets a description of the enemy', () => {
-  const enemy = new Enemy('goblin', 'sword');
-
-  expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
-  expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
-});
-
-test('checks if enemy is alive or not', () => {
-  const enemy = new Enemy('goblin', 'sword');
-
-  expect(enemy.isAlive()).toBeTruthy();
-
-  enemy.health = 0;
-
-  expect(enemy.isAlive()).toBeFalsy();
-});
-
-test("gets enemy's attack value", () => {
-  const enemy = new Enemy('goblin', 'sword');
-  enemy.strength = 10;
-
-  expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
-  expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
-});
-
-test("subtracts from enemy's health", () => {
-  const enemy = new Enemy('goblin', 'sword');
-  const oldHealth = enemy.health;
-
-  enemy.reduceHealth(5);
-
-  expect(enemy.health).toBe(oldHealth - 5);
-
-  enemy.reduceHealth(99999);
-
-  expect(enemy.health).toBe(0);
-});
-//office number
